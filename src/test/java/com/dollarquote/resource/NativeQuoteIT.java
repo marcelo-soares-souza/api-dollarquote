7a3b1ae2f83b09f7131f98bb9df6cd39b850ffb4
Marcelo Soares Souza <marcelo@riseup.net>
2020-03-14 08:40:38 -0300

    Bootstrapping the dollar's quote API project

---
+++ src/test/java/com/dollarquote/resource/NativeQuoteIT.java
@@ -0,0 +1,9 @@
+package com.dollarquote.resource;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeQuoteIT extends QuoteTest {
+
+    // Execute the same tests but in native mode.
+}
