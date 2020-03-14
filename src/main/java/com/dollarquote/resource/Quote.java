3783167edf17b74db9d94e4c92621e825517e9b3
Marcelo Soares Souza <marcelo@riseup.net>
2020-03-14 10:25:03 -0300

    Solving second test scenario. If user inform a incorrect date format as a URL parameter return a error.

--- src/main/java/com/dollarquote/resource/Quote.java
+++ src/main/java/com/dollarquote/resource/Quote.java
@@ -1,7 +1,12 @@
 package com.dollarquote.resource;
 
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -10,8 +15,24 @@ import javax.ws.rs.core.Response;
 @Produces(MediaType.APPLICATION_JSON)
 public class Quote {
 
-    @GET
-    public Response quote() {
-        return Response.ok("Error, please inform a date.").status(400).build();
-    }
+	@GET
+	public Response quote() {
+		return Response.ok("Error, please inform a date.").status(400).build();
+	}
+
+	@GET
+	@Path("{date}")
+	public Response quote(@PathParam("date") String date) {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+		sdf.setLenient(false);
+
+		try {
+			Date dt = sdf.parse(date);
+			return Response.ok("Ok with date " + dt).status(400).build();
+
+		} catch (ParseException e) {
+			return Response.ok("Error, incorrect date format").status(400).build();
+		}
+	}
 }
